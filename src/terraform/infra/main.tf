9bb73396dec7d3a9613347754a46de8af1332955
Fraser Davidson <fraser@frontierdigital.net>
2023-07-19 17:05:37 +0100

    WIP [major]

--- src/terraform/infra/main.tf
+++ src/terraform/infra/main.tf
@@ -2,16 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-# module "resource_group" {
-#   source = "git::https://github.com/frontierdigital/resource-group-terraform-module//src?ref=v0.0.1-pre.4266458"
+module "resource_group" {
+  source = "git::https://github.com/frontierdigital/resource-group-terraform-module//src?ref=v1.0.0-pre.b790a21"
 
-#   environment      = var.environment
-#   identifier       = local.identifier
-#   location         = var.location
-#   set              = var.set
-#   tags             = var.tags
-#   workload_name    = var.workload_name
-#   workload_type    = var.workload_type
-#   workload_version = var.workload_version
-#   zone             = var.zone
-# }
+  environment      = var.environment
+  identifier       = local.identifier
+  location         = var.location
+  tags             = var.tags
+  workload_name    = var.workload_name
+  workload_type    = var.workload_type
+  workload_version = var.workload_version
+  zone             = var.zone
+}
