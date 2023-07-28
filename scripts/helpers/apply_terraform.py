a6feb0d43ca5ba58448a57ce948c71f5df569534
Fraser Davidson <fraser@frontierdigital.net>
2023-07-28 11:37:53 +0100

    WIP

--- scripts/helpers/apply_terraform.py
+++ scripts/helpers/apply_terraform.py
@@ -1,10 +1,9 @@
-# import json
 import os
 import shutil
-# from helpers.exec import exec
 from helpers.get_short_region import get_short_region
+from helpers.get_terraform_output import get_terraform_output
 from helpers.get_terraform_state_config import get_terraform_state_config
-from python_terraform import IsFlagged, Terraform
+from python_terraform import Terraform
 from tempfile import TemporaryDirectory
 
 
@@ -75,34 +74,7 @@ def apply_terraform(
 
     shutil.rmtree(temp_dir_path.name)
 
-    return terraform.output(json=IsFlagged)
-    #
-    # Traceback (most recent call last):
-    # File "/home/runner/work/rancher-cluster-workload/rancher-cluster-workload/scripts/deploy.py", line 22, in <module>
-    #     deploy()
-    # File "/home/runner/work/rancher-cluster-workload/rancher-cluster-workload/scripts/deploy.py", line 8, in deploy
-    #     apply_terraform(
-    # File "/home/runner/work/rancher-cluster-workload/rancher-cluster-workload/scripts/helpers/apply_terraform.py", line 76, in apply_terraform
-    #     return terraform.output()
-    #         ^^^^^^^^^^^^^^^^^^
-    # File "/home/runner/.local/share/virtualenvs/rancher-cluster-workload-fE1wvjYY/lib/python3.11/site-packages/python_terraform/__init__.py", line 360, in output
-    #     value = json.loads(out)
-    #             ^^^^^^^^^^^^^^^
-    # File "/opt/hostedtoolcache/Python/3.11.4/x64/lib/python3.11/json/__init__.py", line 346, in loads
-    #     return _default_decoder.decode(s)
-    #         ^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # File "/opt/hostedtoolcache/Python/3.11.4/x64/lib/python3.11/json/decoder.py", line 337, in decode
-    #     obj, end = self.raw_decode(s, idx=_w(s, 0).end())
-    #             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # File "/opt/hostedtoolcache/Python/3.11.4/x64/lib/python3.11/json/decoder.py", line 355, in raw_decode
-    #     raise JSONDecodeError("Expecting value", s, err.value) from None
-    # json.decoder.JSONDecodeError: Expecting value: line 1 column 2 (char 1)
-
-    # out = exec(
-    #     command="terraform output -json",
-    #     opts={"cwd": working_dir},
-    # )
-    # return json.loads(out[0].decode("utf-8"))
+    return get_terraform_output(terraform=terraform)
 
 
 def _test():
